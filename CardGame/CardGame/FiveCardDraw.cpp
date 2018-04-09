773637a9ec32d8011528b4fdf206271f004c0a76
Shane Blair <sblair@wustl.edu>
2018-04-09 01:17:42 -0500

    Fix discard_deck not emptying.
    Still has DEBUG statements

--- CardGame/CardGame/FiveCardDraw.cpp
+++ CardGame/CardGame/FiveCardDraw.cpp
@@ -226,13 +226,23 @@ int FiveCardDraw::after_round()
 		for (int index = ptr_vector[i]->hand.size() - 1; index >= 0 && index < ptr_vector[i]->hand.size(); --index) {
 			Card card = ptr_vector[i]->hand.remove_card(index);
 			draw_deck.add_card(card);
+			cout << "Discarded card: " << card << endl;
 		}
 		cout << "Discarded hand: " << endl;
 		cout << ptr_vector[i]->hand << endl;
 	}
 
+	cout << "Draw Deck 1: " << endl;
+	cout << draw_deck << endl;
+	cout << "Discard Deck 1: " << endl;
+	cout << discard_deck << endl;
 	//Take cards from discard deck and throw them into the draw deck
 	draw_deck.deck.insert(draw_deck.deck.end(), discard_deck.deck.begin(), discard_deck.deck.end());
+	discard_deck = Deck();
+	cout << "Draw Deck 2" << endl;
+	cout << draw_deck << endl;
+	cout << "Discard Deck 1: " << endl;
+	cout << discard_deck << endl;
 
 	//Ask users if they want to leave
 	string input;
